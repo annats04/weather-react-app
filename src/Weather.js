10b105b6521082a422d9aae08291cc85a262d142
annats04 <ann_tsant@hotmail.com>
2023-02-16 13:34:06 +0100

    weather search engine

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 export default function Weather() {
-  function handleResponse(response) {
-    alert(`the weather in Paris is ${response.data.temperature.current}`);
+  let [city, setCity] = useState(" ");
+  let [loaded, setLoaded] = useState(false);
+  let [weather, setWeather] = useState(" ");
+
+  function weatherInfo(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.temperature.current,
+      wind: response.data.wind.speed,
+      humidity: response.data.temperature.humidity,
+    });
   }
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Paris&key=a5fa44f8460fdadf371tbdae4e7839fo&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-  return <h2> Hello World</h2>;
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=a5fa44f8460fdadf371tbdae4e7839fo&units=metric`;
+    axios.get(apiUrl).then(weatherInfo);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <div>
+      <h2> Weather Search Engine </h2>
+      <form onSubmit={handleSubmit}>
+        <input type="search" placeholder="type a city" onChange={updateCity} />
+        <button type="Submit">Search</button>
+      </form>
+    </div>
+  );
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
