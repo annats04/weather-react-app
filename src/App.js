10781e9ae031a0690b4506d8c032fff8ad8e4fab
annats04 <ann_tsant@hotmail.com>
2023-02-28 16:10:33 +0100

    setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
+import React from "react";
 import "./App.css";
-import Weather from "./Weather";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1> Weather App</h1>
-        <Weather />
-      </header>
-    </div>
-  );
+export default function App() {
+  return <div className="App">weather app</div>;
 }
-
-export default App;
