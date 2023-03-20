0b1c6fb0869516a5b828537612a2dc742b276da0
annats04 <ann_tsant@hotmail.com>
2023-03-20 14:48:31 +0100

    finalized

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -13,8 +13,8 @@ export default function WeatherInfo(props) {
       <h1 className="city">{props.data.city} </h1>
       <div className="container">
         <div className="row justify-content-start">
-          <div className="col-sm-4 mx-auto">
-            <div className="d-flex">
+          <div className="col-sm-5 mx-auto">
+            <div className="d-flex justify-content-center">
               <WeatherIcon
                 code={props.data.icon}
                 alt={props.data.description}
@@ -33,11 +33,11 @@ export default function WeatherInfo(props) {
         <div className="row mt-5 mb-3">
           <div className="col realfeel">
             {" "}
-            <i className="fa-solid fa-temperature-low"></i> realFeel:
+            <i className="fa-solid fa-temperature-low"></i> realFeel:{" "}
             {Math.round(props.data.feelsLike)}°C
           </div>
           <div className="col humidity">
-            <i className="fa-solid fa-droplet"></i> humidity:
+            <i className="fa-solid fa-droplet"></i> humidity:{" "}
             {props.data.humidity}%
           </div>
           <div className="col wind">
